e2dcfecd7073bf7e2b175bdab3310d3b07f04db6
tanaymittal25 <tanay.mittal25@gmail.com>
2019-07-11 13:51:17 +0530

    index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
-            <button className="square">
+            <button className="square" 
+                    onClick={() => this.setState({value: 'X'})}>
                 {
-
+                    this.state.value
                 }
             </button>
         );
@@ -12,7 +23,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square />
+        return <Square value={i} />;
     }
 
     render() {
